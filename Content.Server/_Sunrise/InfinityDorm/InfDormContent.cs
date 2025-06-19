66a05252b2ba8ac41b8482fb45c7ecdb88d421af
BTReflex <BTReflex@yandex.ru>
2025-06-19 14:58:24 +0600

    infdormspawner

---
+++ Content.Server/_Sunrise/InfinityDorm/InfDormContent.cs
@@ -0,0 +1,8 @@
+using Content.Shared.DoAfter;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+
